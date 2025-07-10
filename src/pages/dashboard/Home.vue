6ae86bf515ed7caeac8dc610dbcd7535319554d5
Alameen Azad <alameen.azad@protonmail.com>
2025-07-10 14:49:19 +0300

    feat: implement Import Funding feature with file upload and progress tracking

--- src/pages/dashboard/Home.vue
+++ src/pages/dashboard/Home.vue
@@ -2,15 +2,8 @@
   <q-page class="q-mt-lg" :class="$q.screen.gt.sm ? 'q-mx-xl' : 'q-mx-sm'">
     <div class="row q-col-gutter-md">
       <div class="col-12 col-md-3">
-        <q-btn
-          color="blue"
-          icon="search"
-          unelevated
-          class="mr-0 radius-6 text-weight-600"
-          :class="{ 'full-width': $q.screen.lt.md }"
-          no-caps
-          to="/overview"
-        >
+        <q-btn color="blue" icon="search" unelevated class="mr-0 radius-6 text-weight-600"
+          :class="{ 'full-width': $q.screen.lt.md }" no-caps to="/overview">
           <p class="q-mb-none q-mx-md q-my-sm">
             {{ $t("homeBtns.SearchBtn") }}
           </p>
@@ -18,49 +11,25 @@
       </div>
       <div class="col-12 col-md-9">
         <div class="row q-col-gutter-md justify-end">
-          <div class="col-12 col-md-auto" v-if="!isGuest">
-            <q-btn
-              color="blue"
-              icon="add"
-              unelevated
-              outline
-              class="mr-0 radius-6 text-weight-600"
-              :class="{ 'full-width': $q.screen.lt.md }"
-              no-caps
-              @click="$router.push({ path: '/user/newProjectIdea' })"
-            >
+          <div class="col-12 col-md-auto" v-if="isAdmin">
+            <q-btn color="blue" icon="drive_folder_upload" unelevated outline class="mr-0 radius-6 text-weight-600"
+              :class="{ 'full-width': $q.screen.lt.md }" no-caps @click="openImportFunding">
               <p class="q-mb-none q-mx-md q-my-sm">
-                {{ $t("homeBtns.projectBtn") }}
+                {{ $t("homeBtns.importFundingBtn") }}
               </p>
             </q-btn>
           </div>
           <div v-if="isAdmin" class="col-12 col-md-auto">
-            <q-btn
-              color="blue"
-              icon="add"
-              unelevated
-              outline
-              class="mr-0 radius-6 text-weight-600"
-              :class="{ 'full-width': $q.screen.lt.md }"
-              no-caps
-              @click="$router.push({ path: '/user/newFunding' })"
-            >
+            <q-btn color="blue" icon="add" unelevated outline class="mr-0 radius-6 text-weight-600"
+              :class="{ 'full-width': $q.screen.lt.md }" no-caps @click="$router.push({ path: '/user/newFunding' })">
               <p class="q-mb-none q-mx-md q-my-sm">
                 {{ $t("homeBtns.fundingsBtn") }}
               </p>
             </q-btn>
           </div>
           <div class="col-12 col-md-auto" v-if="!isGuest">
-            <q-btn
-              color="blue"
-              icon="add"
-              unelevated
-              outline
-              class="mr-0 radius-6 text-weight-600"
-              :class="{ 'full-width': $q.screen.lt.md }"
-              no-caps
-              @click="$router.push({ path: '/user/newChecklist' })"
-            >
+            <q-btn color="blue" icon="add" unelevated outline class="mr-0 radius-6 text-weight-600"
+              :class="{ 'full-width': $q.screen.lt.md }" no-caps @click="$router.push({ path: '/user/newChecklist' })">
               <p class="q-mb-none q-mx-md q-my-sm">
                 {{ $t("homeBtns.checkListBtn") }}
               </p>
@@ -85,6 +54,9 @@
     <div v-if="false">
       <network />
     </div>
+    <div v-if="isAdmin">
+      <importFunding :showDialog="showImportFunding" @update:showDialog="showImportFunding = $event" />
+    </div>
   </q-page>
 </template>
 
@@ -94,6 +66,8 @@ import dataOverview from "components/data/overview.vue";
 import myCommunityData from "components/data/myCommunityData.vue";
 import watchlist from "components/watchlist/overview.vue";
 import network from "components/network/overview.vue";
+import importFunding from "components/funding/ImportFunding.vue";
+
 export default {
   name: "index",
   components: {
@@ -102,9 +76,12 @@ export default {
     myCommunityData,
     watchlist,
     network,
+    importFunding
   },
   data() {
-    return {};
+    return {
+      showImportFunding: false,
+    };
   },
   computed: {
     isAdmin() {
@@ -112,6 +89,11 @@ export default {
     },
     isGuest() {
       return this.$store.getters["userCenter/isGuest"];
+    },
+  },
+  methods: {
+    openImportFunding() {
+      this.showImportFunding = true;
     },
   },
 };
