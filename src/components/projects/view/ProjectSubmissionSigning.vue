2324074534cb681d7faa62edd4d516aeace5a8fa
Alameen Azad <alameen.azad@protonmail.com>
2025-09-03 21:55:14 +0300

    feat: add ProjectSubmissionSigning component for displaying project submission signing details

---
+++ src/components/projects/view/ProjectSubmissionSigning.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-card class="shadow-1 radius-20">
+    <q-expansion-item class="shadow-1 overflow-hidden radius-20"
+      :label="$t('projectComponents.submissionSigning.title')" header-class="bg-white text-black"
+      v-model="expandedSubmissionSigning">
+      <q-card-section class="q-pt-none">
+        <h4 class="font-16 text-blue-grey-10 q-mb-none q-mt-none">
+          {{ $t('projectComponents.submissionSigning.description') }}
+        </h4>
+
+        <div class="col-12 q-mt-md">
+          <h5 class="font-16 text-blue-grey-10 q-mb-md">
+            {{ $t('projectComponents.submissionSigning.selectionLabel') }}
+          </h5>
+
+          <div class="row q-col-gutter-md items-center">
+            <div class="col-auto">
+              <q-icon :name="submissionStatus === null ? 'help_outline' : (submissionStatus ? 'check' : 'clear')"
+                :color="submissionStatus === null ? 'grey' : (submissionStatus ? 'green' : 'red')" size="xl" />
+            </div>
+            <div class="col-auto">
+              <span v-if="submissionStatus === null">
+                {{ $t('projectComponents.submissionSigning.pendingDecision') }}
+              </span>
+              <span v-else-if="submissionStatus === true">
+                {{ $t('projectComponents.submissionSigning.grantNotice') }}
+              </span>
+              <span v-else>
+                {{ $t('projectComponents.submissionSigning.rejectionNotice') }}
+              </span>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+    </q-expansion-item>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "ProjectSubmissionSigning",
+  props: {
+    project: {
+      type: Object,
+      required: true
+    },
+    currentTab: {
+      type: String,
+      default: null,
+      required: false
+    }
+  },
+  data() {
+    return {
+      expandedSubmissionSigning: this.currentTab === "submissionSigning",
+    };
+  },
+  computed: {
+    submissionStatus() {
+      return this.project && this.project.status !== undefined
+        ? this.project.status
+        : null;
+    }
+  },
+  watch: {
+    currentTab(newTab) {
+      // Expand the section if the current tab is 'submissionSigning'
+      this.expandedSubmissionSigning = newTab === "submissionSigning";
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.text-block {
+  white-space: pre-line;
+}
+</style>
