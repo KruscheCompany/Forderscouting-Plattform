10d02ea042167a963e4a07158b8ebb9140adedd4
Ameen Azad <alameen.azad@protonmail.com>
2025-09-03 22:41:39 +0300

    Revert "feat: allow admins to transfer from all users"

--- src/components/user/settings/TransferDialog.vue
+++ src/components/user/settings/TransferDialog.vue
@@ -5,19 +5,52 @@
         <h6 class="font-16 q-ma-none">{{ $t("generalData.transferData") }}</h6>
       </q-card-section>
       <q-card-section class="q-pa-none ">
-        <q-stepper v-model="step" ref="stepper" color="primary" class="shadow-0" :contracted="$q.screen.lt.md" animated>
-          <q-step style="min-height: 50vh" :name="1" :title="$t('generalData.selectUser')" icon="person_outline"
-            :done="step > 1">
-            <q-input borderless outlined class="input-radius-6 no-shadow q-mb-sm q-mt-sm" v-model="filter"
-              :placeholder="$t('Search')" dense role="searcbox">
+        <q-stepper
+          v-model="step"
+          ref="stepper"
+          color="primary"
+          class="shadow-0"
+          :contracted="$q.screen.lt.md"
+          animated
+        >
+          <q-step
+            style="min-height: 50vh"
+            :name="1"
+            :title="$t('generalData.selectUser')"
+            icon="person_outline"
+            :done="step > 1"
+          >
+            <q-input
+              borderless
+              outlined
+              class="input-radius-6 no-shadow q-mb-sm q-mt-sm"
+              v-model="filter"
+              :placeholder="$t('Search')"
+              dense
+              role="searcbox"
+            >
               <template v-slot:prepend>
                 <q-icon color="blue-5" name="search" />
               </template>
             </q-input>
-            <q-list v-if="!!filteredUsers && filteredUsers.length > 0" class="radius-6" bordered separator>
-              <q-item @click="form.selectedUser = user.id" v-for="user in filteredUsers" :key="user.id" clickable
-                v-ripple :active="form.selectedUser === user.id" active-class="text-weight-600 text-primary">
-                <q-item-section v-if="!!filteredUsers && filteredUsers.length > 0">
+            <q-list
+              v-if="!!filteredUsers && filteredUsers.length > 0"
+              class="radius-6"
+              bordered
+              separator
+            >
+              <q-item
+                @click="form.selectedUser = user.id"
+                v-for="user in filteredUsers"
+                :key="user.id"
+                clickable
+                v-ripple
+                :active="form.selectedUser === user.id"
+                active-class="text-weight-600 text-primary"
+              >
+                <q-item-section
+                  v-if="!!filteredUsers && filteredUsers.length > 0"
+                >
                   <div class="row">
                     <div class="col-12">
                       <span class="text-grey-7">
@@ -25,7 +58,7 @@
                           (!!user.user_detail &&
                             !!user.user_detail.municipality &&
                             user.user_detail.municipality.title) ||
-                          ""
+                            ""
                         }}
                       </span>
                       <span> - {{ user.username }} </span>
@@ -36,12 +69,18 @@
             </q-list>
             <q-list v-else class="radius-6" bordered separator>
               <q-item>
-                <q-item-section>{{ $t('No User') }}</q-item-section>
+                <q-item-section>{{$t('No User')}}</q-item-section>
               </q-item>
             </q-list>
           </q-step>
 
-          <q-step style="min-height: 50vh" :name="2" :title="$t('Select Data')" icon="account_tree" :done="step > 2">
+          <q-step
+            style="min-height: 50vh"
+            :name="2"
+            :title="$t('Select Data')"
+            icon="account_tree"
+            :done="step > 2"
+          >
             <div class="row">
               <div class="col-12">
                 <p class="font-16 ">
@@ -51,29 +90,65 @@
             </div>
             <div class="row">
               <div class="col-6">
-                <q-checkbox v-model="form.data" val="Project" :label="$t('myData.projectIdeas')" color="primary" />
+                <q-checkbox
+                  v-model="form.data"
+                  val="Project"
+                  :label="$t('myData.projectIdeas')"
+                  color="primary"
+                />
               </div>
               <div class="col-6">
-                <q-checkbox v-model="form.data" val="Funding" :label="$t('myData.fundings')" color="primary" />
+                <q-checkbox
+                  v-model="form.data"
+                  val="Funding"
+                  :label="$t('myData.fundings')"
+                  color="primary"
+                />
               </div>
               <div class="col-6">
-                <q-checkbox v-model="form.data" val="Checklist" :label="$t('myData.implementationChecklist')"
-                  color="primary" />
+                <q-checkbox
+                  v-model="form.data"
+                  val="Checklist"
+                  :label="$t('myData.implementationChecklist')"
+                  color="primary"
+                />
               </div>
               <div class="col-6">
-                <q-checkbox v-model="form.data" val="Watchlist" :label="$t('Statistics.watchList')" color="primary" />
+                <q-checkbox
+                  v-model="form.data"
+                  val="Watchlist"
+                  :label="$t('Statistics.watchList')"
+                  color="primary"
+                />
               </div>
             </div>
           </q-step>
 
           <template v-slot:navigation>
             <q-stepper-navigation class="flex ">
-              <q-btn v-if="step > 1" outline color="primary" @click="$refs.stepper.previous()" :label="$t('Back')"
-                class="q-ml-sm" />
+              <q-btn
+                v-if="step > 1"
+                outline
+                color="primary"
+                @click="$refs.stepper.previous()"
+                :label="$t('Back')"
+                class="q-ml-sm"
+              />
               <q-space />
-              <q-btn v-if="step > 1" @click="transferData" color="primary" :label="$t('generalData.transferData')" />
-              <q-btn :disable="!form.selectedUser" v-if="step === 1" @click="$refs.stepper.next()" color="primary"
-                :label="$t('Select Data')" :loading="isLoading" />
+              <q-btn
+                v-if="step > 1"
+                @click="transferData"
+                color="primary"
+                :label="$t('generalData.transferData')"
+              />
+              <q-btn
+                :disable="!form.selectedUser"
+                v-if="step === 1"
+                @click="$refs.stepper.next()"
+                color="primary"
+                :label="$t('Select Data')"
+                :loading="isLoading"
+              />
             </q-stepper-navigation>
           </template>
         </q-stepper>
@@ -125,61 +200,32 @@ export default {
         !!this.$store.state.userCenter.user && this.$store.state.userCenter.user
       );
     },
-    isAdmin() {
-      return this.$store.getters["userCenter/isAdmin"];
-    },
     users() {
-      // If user is admin, return all users except self
-      if (this.isAdmin) {
-        const referenceUser = this.fromId?.id ? this.fromId : this.currentUser;
-        const againstUserID = referenceUser?.id ||
-          (referenceUser?.user?.id ? Number(referenceUser.user.id) : null);
-
-        return this.$store.state.userCenter.users.filter(user =>
-          user.id !== againstUserID
+      let againstUserID =
+        !!this.currentUser &&
+        !!this.currentUser.user &&
+        this.currentUser.user.id;
+      let againstUserMunicipalityID =
+        !!this.currentUser &&
+        !!this.currentUser.userDetails &&
+        !!this.currentUser.userDetails.municipality &&
+        this.currentUser.userDetails.municipality.id;
+      if (!!this.fromId && !!this.fromId.id) {
+        againstUserID = Number(this.fromId.id);
+        againstUserMunicipalityID = Number(
+          !!this.fromId &&
+            !!this.fromId.user_detail &&
+            !!this.fromId.user_detail.municipality &&
+            this.fromId.user_detail.municipality.id
         );
       }
-
-      // Get reference user (either fromId or currentUser)
-      const referenceUser = this.fromId?.id ? this.fromId : this.currentUser;
-
-      // Extract user ID
-      const againstUserID = referenceUser?.id ||
-        (referenceUser?.user?.id ? Number(referenceUser.user.id) : null);
-
-      // Extract municipality ID
-      const municipalityPath = referenceUser?.id ?
-        'user_detail.municipality.id' :
-        'userDetails.municipality.id';
-
-      const againstUserMunicipalityID = this.getNestedProperty(referenceUser, municipalityPath);
-
-      // If no valid municipality ID is found, return empty array
-      if (!againstUserMunicipalityID) return [];
-
-      // Filter users
-      return this.$store.state.userCenter.users.filter(user =>
-        user.id !== againstUserID &&
-        user.user_detail?.municipality?.id === againstUserMunicipalityID
+      return this.$store.state.userCenter.users.filter(
+        user =>
+          user.id != againstUserID &&
+          !!user.user_detail &&
+          !!user.user_detail.municipality &&
+          user.user_detail.municipality.id === againstUserMunicipalityID
       );
-    },
-
-    // Helper function to safely access nested properties
-    getNestedProperty(obj, path) {
-      if (!obj) return null;
-
-      const properties = path.split('.');
-      let value = obj;
-
-      for (const prop of properties) {
-        if (value && value[prop] !== undefined) {
-          value = value[prop];
-        } else {
-          return null;
-        }
-      }
-
-      return typeof value === 'string' ? Number(value) : value;
     },
     filteredUsers() {
       return this.users.filter(user => {
@@ -187,10 +233,10 @@ export default {
       });
     },
     $_options: {
-      get: function () {
+      get: function() {
         return this.dialogState;
       },
-      set: function (val) {
+      set: function(val) {
         this.form.selectedUser = null;
         this.form.data = [];
         this.active = false;
